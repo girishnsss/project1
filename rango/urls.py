ec72128246a176504b6fe4413e4c5042e7ed5f22
girishnsss <girishkumar.nistala@gmail.com>
2017-06-01 12:43:44 +0530

    first commit

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
 url(r'^$', views.index, name='index'),url(r'about/', views.about, name='about')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
